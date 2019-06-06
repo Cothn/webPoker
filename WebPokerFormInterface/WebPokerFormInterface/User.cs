3a7c1b2d6f2a4b6d88e7ae721aa79ef66811e714
Cothn <Sevkaa-0013@mail.ru>
2019-06-06 16:16:29 +0400

    Form

---
+++ WebPokerFormInterface/WebPokerFormInterface/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebPokerFormInterface
+{
+    class User
+    {
+        public string login = "";
+        public string name = "";
+
+        public User(string log, string nameUser)
+        {
+            login = log;
+            name = nameUser;
+        }
+    }
+    public class Player
+    {
+        public string login;
+        public string name;
+        public string card1;
+        public string card2;
+        public int money;
+        public bool fold = false;
+        public int bet;
+        public int MaxBet;
+        public int Allmoney;
+        public string table = "";
+    }
+}
